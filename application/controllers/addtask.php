ff3ab465844be7b9daa7ca9583efbaa8ddf0d261
lovrenca <a.lovrenc@gmail.com>
2013-04-08 19:29:19 +0200

    napredek,  a še ne dela

--- application/controllers/addtask.php
+++ application/controllers/addtask.php
@@ -1,1 +1,34 @@
 <!--avtor:Lovrenc-->
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class addTask extends CI_Controller {
+
+	public function __construct()	{
+		parent::__construct();
+	}
+
+	public function index()	{
+		if($this->session->userdata('logged_in')) {
+			$session_data = $this->session->userdata('logged_in');
+			$data['username'] = $session_data['username'];
+			$data['name'] = $session_data['name'];
+			$data['rights'] = $session_data['rights'];
+				if(strcmp($data['rights'],'user')==0) 
+				{
+					redirect('home','refresh');
+				}
+			$data['active']='addtask';
+			$this->load->model("stories");
+			$data['stories']= $this->stories->getAll();			
+			$this->load->view('header', $data);
+			$this->load->helper(array('form'));
+			$this->load->view('addtask_view',$data);
+			$this->load->view('footer');
+		} 
+		else{
+			redirect('login', 'refresh');
+		}
+
+	}
+
+}
