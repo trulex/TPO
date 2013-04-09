c41fdbc5b60f65f8c3e4bbe9bdc72cc23f9995d4
lovrenca <a.lovrenc@gmail.com>
2013-04-09 19:09:51 +0200

    naredil seznam zgodb bolj pregleden

--- application/views/viewTasks.php
+++ application/views/viewTasks.php
@@ -4,12 +4,12 @@
 
 <?php foreach ($stories as $story): ?>
 	<h2> <?php echo $story->id.".)".$story->name ?></h2>
-	<div> <?php echo $story->text ?>
-	<?php foreach ($tasks as $task): 
+	<div> <?php echo '"'.$story->text.'"'?>
+	<ul><?php foreach ($tasks as $task): 
 	if($story->id==$task->StID){?>
-		<h3><?php echo $task->task_name ?></h3>
-		<div><?php echo $task->text ?></div>
-	<?php } endforeach?></div>
+		<li><h3><?php echo $task->task_name ?></h3>
+		<div><?php echo '"'.$task->text.'"' ?></div></li>
+	<?php } endforeach?></div></ul>
 <?php endforeach ?>
 
     
