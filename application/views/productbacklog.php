64b7cb176bc757c2feb91c1525eb54d3c0971f97
lovrenca <a.lovrenc@gmail.com>
2013-04-12 14:47:12 +0200

    problemi....

--- application/views/productbacklog.php
+++ application/views/productbacklog.php
@@ -9,7 +9,6 @@
 	    <li><?php echo anchor('addsprint','Add new sprints'); ?></li>
 	    <li><?php echo anchor('viewTasks','View all Tasks'); ?></li>
 	    <li><?php echo anchor('sprintBacklog','Sprint backlog-za enkrat vsi storyji trenutnega projekta'); ?></li>
-	    <li><?php echo anchor('verifyAddTask','add Task'); ?></li>
 	</ul>
     </div>
     <div id="projects">
