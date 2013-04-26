ff34f4f47ffae0f77349b692c9fbc27ef3f00a4b
Boštjan Čotar <boco_13@hotmail.com>
2013-04-26 13:45:17 +0200

    USER FRIENDLY HOME VIEW

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,8 +1,9 @@
 <div id="content">
-	<div id=left>
+	<div id="left">
     <h1>Home</h1>
     <h2>Welcome <?php echo $username; ?>!</h2>
-     Get your shit together it's time to work, pick a project and SCRUMMMMMMmmmm!!!!
+     Get your shit together it's time to work, pick a project and SCRUMMMMMMmmmm!!!! <br>
+	<img border="0" src="/pics/magic.jpg" alt="Pulpit rock" width="460" height="1335">
      </div>
 	<?php $this->load->view('selProject', array('projects'=>$projects));   ?>
 </div>
