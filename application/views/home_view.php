73f3debd03d43d643805a7145e8a416ff4527529
Lovrenc Avsenek <a.lovrenc@gmail.com>
2013-05-01 10:44:43 +0200

    minor fix(the immage path made relative(was absolute))

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,7 +3,7 @@
     <h2>Home</h2>
     <h3>Welcome <?php echo $username; ?>!</h3>
     Get your shit together it's time to work, pick a project and SCRUMMMMMMmmmm!!!! <br>
-	<img border="0" src="/pics/magic.jpg" alt="Pulpit rock" width="460" height="1335">
+	<img border="0" src="../pics/magic.jpg" alt="Pulpit rock" width="460" height="1335">
      </div>
 	<?php $this->load->view('selProject', array('projects'=>$projects));   ?>
 </div>
